c7a09edd566d874793d04a2e63a1be629d78be2f
Katerina Kotseva <k.kotseva@xs-software.com>
2026-07-01 09:01:49 +0300

    Refactoring

--- game/game_controller.py
+++ game/game_controller.py
@@ -2,7 +2,13 @@
 
 import pygame
 
+
+from game.player import Player
+from game.enemy import Enemy
+from game.game_data import Position
 from game.game_renderer import GameRenderer
+from game.maps import GameMap, LEVEL_1
+from game.assets_manager import AssetManager
 from game.settings import FPS, SCREEN_HEIGHT, SCREEN_WIDTH, WINDOW_TITLE, MAP_OFFSET_Y, MAP_OFFSET_X, TILE_SIZE
 
 class GameController:
@@ -11,9 +17,14 @@ class GameController:
 
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption(WINDOW_TITLE)
-
+        self.assets_manager = AssetManager()
         self.game_renderer = GameRenderer(self.screen)
-
+        self.game_map = GameMap(LEVEL_1, self.assets_manager)
+        start_position = self.game_map.get_player_start_position()
+        self.player = Player(start_position)
+        self.mercury = Enemy(start_position, 0)
+        self.mars = Enemy(start_position, 1)
+        self.venus = Enemy(start_position, 2)
         self.clock = pygame.time.Clock()
         self.running = True
 
@@ -21,8 +32,8 @@ class GameController:
             while self.running:
                 dt = self.clock.tick(FPS)
                 self.handle_events()
-                self.game_renderer.update(dt)
-                self.game_renderer.draw()
+                self.game_renderer.update(dt, self.player)
+                self.game_renderer.draw(self.player)
                 self.clock.tick(FPS)
 
             pygame.quit()
@@ -32,7 +43,34 @@ class GameController:
             if event.type == pygame.QUIT:
                 self.running = False
             elif event.type == pygame.KEYDOWN:
-                self.game_renderer.move_player(event)
+                self.move_player(event)
             elif event.type == pygame.KEYUP:
-                self.game_renderer.player.set_animation("player_idle")
+                self.player.set_animation("player_idle")
+
+    def move_player(self, event):
+        move_x = 0
+        move_y = 0
+
+        if event.key == pygame.K_UP:
+            move_y = -1
+            self.player.set_animation("player_walk_up")
+        elif event.key == pygame.K_DOWN:
+            move_y = 1
+            self.player.set_animation("player_walk_down")
+        elif event.key == pygame.K_LEFT:
+            move_x = -1
+            self.player.set_animation("player_walk_left")
+        elif event.key == pygame.K_RIGHT:
+            move_x = 1
+            self.player.set_animation("player_walk_right")
+        else:
+            return
+
+        new_position = Position(
+            self.player.position.x + move_x,
+            self.player.position.y + move_y,
+        )
+
+        if self.game_map.is_moving(new_position):
+            self.player.move(move_x, move_y)
     
--- game/game_renderer.py
+++ game/game_renderer.py
@@ -1,8 +1,5 @@
 import pygame
 
-from game.player import Player
-from game.enemy import Enemy
-from game.game_data import Position
 from game.assets_manager import AssetManager
 from game.ui_manager import UIManager
 from game.maps import GameMap, LEVEL_1
@@ -14,58 +11,29 @@ class GameRenderer:
         self.assets_manager.load_all()
         self.ui_manager = UIManager(self.assets_manager)
         self.game_map = GameMap(LEVEL_1, self.assets_manager)
-        start_position = self.game_map.get_player_start_position()
-        self.player = Player(start_position)
-        self.mercury = Enemy(start_position, 0)
-        self.mars = Enemy(start_position, 1)
-        self.venus = Enemy(start_position, 2)
+      
         self.screen = screen
 
-    def draw(self):
+    def draw(self, player):
         self.game_map.draw(self.screen)
-        self.draw_player()
+        self.draw_player(player)
         self.ui_manager.draw(self.screen)
       
         pygame.display.flip()
 
-    def draw_player(self):
-        frames = self.assets_manager.get_animation(self.player.current_animation)
-        player_frame = frames[self.player.frame_index]
+    def draw_player(self, player):
+        frames = self.assets_manager.get_animation(player.current_animation)
+        player_frame = frames[player.frame_index]
 
-        x = MAP_OFFSET_X + self.player.position.x * TILE_SIZE
-        y = MAP_OFFSET_Y + self.player.position.y * TILE_SIZE
+        x = MAP_OFFSET_X + player.position.x * TILE_SIZE
+        y = MAP_OFFSET_Y + player.position.y * TILE_SIZE
 
         self.screen.blit(player_frame, (x, y))
 
-    def update(self, dt):
-        frames = self.assets_manager.get_animation(self.player.current_animation)
-        self.player.update_animation(dt, len(frames))
+    def update(self, dt, player):
+        frames = self.assets_manager.get_animation(player.current_animation)
+        player.update_animation(dt, len(frames))
 
-        print(self.player.current_animation, self.player.frame_index)
+        print(player.current_animation, player.frame_index)
     
-    def move_player(self, event):
-        move_x = 0
-        move_y = 0
-
-        if event.key == pygame.K_UP:
-            move_y = -1
-            self.player.set_animation("player_walk_up")
-        elif event.key == pygame.K_DOWN:
-            move_y = 1
-            self.player.set_animation("player_walk_down")
-        elif event.key == pygame.K_LEFT:
-            move_x = -1
-            self.player.set_animation("player_walk_left")
-        elif event.key == pygame.K_RIGHT:
-            move_x = 1
-            self.player.set_animation("player_walk_right")
-        else:
-            return
-
-        new_position = Position(
-            self.player.position.x + move_x,
-            self.player.position.y + move_y,
-        )
-
-        if self.game_map.is_moving(new_position):
-            self.player.move(move_x, move_y)
+    

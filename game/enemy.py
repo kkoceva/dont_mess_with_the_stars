8d1693a1205e0ffc3c652f3ebb2506564151af91
Katerina Kotseva <k.kotseva@xs-software.com>
2026-08-28 22:45:08 +0300

    Enemy update

--- game/enemy.py
+++ game/enemy.py
@@ -1,14 +1,8 @@
 from enum import Enum
 
-class EnemyType(Enum):
-    Mercury = 0,
-    Mars = 1,
-    Venus = 3
-
 class Enemy:
-    def __init__(self, position, enemy_type):
+    def __init__(self, position):
         self.position = position
-        self.enemy_type = enemy_type
 
     def set_animation(self, animation_name):
         if self.current_animation != animation_name:
@@ -16,3 +10,37 @@ class Enemy:
             self.frame_index = 0
             self.animation_timer = 0
 
+    def move(self, move_x, move_y, speed):
+        if self.status.is_alive:
+            self.position.x += move_x
+            self.position.y += move_y
+
+    def update_stats():
+        pass
+
+
+
+
+class Mercury(Enemy):
+    def __init__(self, position):
+        super().__init__(position)
+
+    def move(self, move_x, move_y):
+        super().move(self, move_x, move_y)
+
+
+class Mars(Enemy):
+    def __init__(self, position):
+        super().__init__(position)
+
+    def move(self, move_x, move_y):
+        super().move(self, move_x, move_y)
+
+
+class Venus(Enemy):
+    def __init__(self, position):
+        super().__init__(position)
+
+    def move(self, move_x, move_y):
+        super().move(self, move_x, move_y)
+        
